3bf3e873490deaad0e16dc2b7a6c0ebdb581056b
elecbug <deveb1479@gmail.com>
2024-05-20 22:16:27 +0900

    add search repo

--- CompactGit/Components/Pages/MyRepo.razor.cs
+++ CompactGit/Components/Pages/MyRepo.razor.cs
@@ -10,9 +10,12 @@ namespace CompactGit.Components.Pages
     public partial class MyRepo : ComponentBase
     {
         private bool showTypeDropdown = false;
-        private string FindInput { get; set; } = "";
+        private string findInput = "";
+        private string FindInput { get=>findInput; set { findInput = value; TextOnChange(); } }
+
         private List<RepoData> FilteredRepoList { get; set; } = new List<RepoData>();
         private List<RepoData> RepoList { get; set; } = new List<RepoData>();
+        private List<RepoData> ShowRepoList { get; set; } = new List<RepoData>();
 
         [Parameter]
         public string UserUrl { get; set; } = default!;
@@ -27,6 +30,7 @@ namespace CompactGit.Components.Pages
         {
             RepoList = GetRepos(UserUrl) ?? new List<RepoData>();
             FilteredRepoList = new List<RepoData>(RepoList);
+            ShowRepoList = new List<RepoData>(FilteredRepoList);
 
             await base.OnInitializedAsync();
         }
@@ -46,10 +50,12 @@ namespace CompactGit.Components.Pages
             if (isPublic == null)
             {
                 FilteredRepoList = RepoList.ToList();
+                ShowRepoList = FilteredRepoList.Where(x => x.RepoName.Contains(FindInput)).ToList();
             }
             else
             {
                 FilteredRepoList = RepoList.Where(repo => repo.IsPublic == isPublic).ToList();
+                ShowRepoList = FilteredRepoList.Where(x => x.RepoName.Contains(FindInput)).ToList();
             }
             showTypeDropdown = false;
         }
@@ -94,6 +100,11 @@ namespace CompactGit.Components.Pages
                     return null;
                 }
             }
+        }
+
+        private void TextOnChange()
+        {
+            ShowRepoList = FilteredRepoList.Where(x => x.RepoName.Contains(FindInput)).ToList();
         }
     }
 }
