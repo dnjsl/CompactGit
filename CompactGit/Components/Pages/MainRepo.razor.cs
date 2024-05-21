54ae00688cec8a09bf6e29698c469638b2dfd403
elecbug <deveb1479@gmail.com>
2024-05-21 18:25:21 +0900

    접근 가능한 레포 설정 완료

--- CompactGit/Components/Pages/MainRepo.razor.cs
+++ CompactGit/Components/Pages/MainRepo.razor.cs
@@ -1,4 +1,6 @@
+using CompactGit.Utils;
 using Microsoft.AspNetCore.Components;
+using System.Text.Json;
 
 namespace CompactGit.Components.Pages
 {
@@ -9,5 +11,50 @@ namespace CompactGit.Components.Pages
 
         [Parameter]
         public string RepoUrl { get; set; } = default!;
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = default!;
+
+        [Inject]
+        public ICookie Cookie { get; set; } = default!;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            Initialize();
+
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+        string Hmm { get; set; } = "";
+
+        private async void Initialize()
+        {
+            string user = await Cookie.GetValue("login");
+
+            if (ValidRepo() && (user == UserUrl || IsPublicRepo()))
+            {
+            }
+            else
+            {
+            }
+        }
+
+        private bool ValidRepo()
+        {
+            return Directory.Exists("gits/" + UserUrl + "/" +  RepoUrl);
+        }
+
+        private bool IsPublicRepo()
+        {
+            using (StreamReader sr = new StreamReader("repos/" + UserUrl))
+            {
+                string json = sr.ReadToEnd();
+                List<RepoData> repos = JsonSerializer.Deserialize<List<RepoData>>(json)!;
+
+                RepoData data = repos.Find(x => x.RepoName == RepoUrl)!;
+
+                return data.IsPublic;
+            }
+        }
     }
 }
--- CompactGit/Utils/Cmd.cs
+++ CompactGit/Utils/Cmd.cs
@@ -16,5 +16,28 @@ namespace CompactGit.Utils
             process.StartInfo = startInfo;
             process.Start();
         }
+
+        public static string CallCmdAndOutput(string command)
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C " + command + " >> tmp.txt";
+
+            process.StartInfo = startInfo;
+            process.Start();
+
+            string result;
+
+            using (StreamReader reader = new StreamReader("tmp.txt"))
+            {
+                result = reader.ReadToEnd();
+            }
+            File.Delete("tmp.txt");
+
+            return result;
+        }
     }
 }
